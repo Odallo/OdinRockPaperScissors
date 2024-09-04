0cdced9ba33dd8fb0b460d9ac5f750f1973603d8
Odallo <eugeneodali181@gmail.com>
2024-09-04 18:58:14 +0300

    did the bugs

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function computerChoice(){
     let random = Math.floor(Math.random() * 3);
     if (random === 0){
         return "rock";
-    }else if(random > 0 < 1){
+    }else if(random === 1){
         return "paper";
     }else{
         return "scissors";
@@ -10,8 +10,39 @@ function computerChoice(){
 }
 
 function humanChoice(){
-    let input = parseInt(window.prompt("What is your choice: "));
-    return input;
+    let input = window.prompt("What is your choice: ");
+    return input.toLowerCase;
 }
 
 console.log (humanChoice());
+
+const humanScore = 0;
+const computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === computerChoice) {
+        return "It's a tie!";
+        } else if ((humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")) {
+            return "You win this round!";
+            } else {
+                return "You lose this round!";
+                }
+}
+const humanSelection = humanChoice();
+const computerSelection = computerChoice();
+
+function playGame(){
+    for (let i = 0; i < 5; i++){
+        let humanSelection = humanChoice();
+        let computerSelection = computerChoice();
+
+        let result = playRound(humanSelection, computerSelection);
+        console.log(`Round ${i+1}: Human chose ${humanSelection}, Computer chose ${computerSelection}. ${result}`);
+    }
+}
+
+playGame();
+
+console.log(`Round ${i+1}: Human chose ${humanSelection}, Computer chose ${computerSelection}. ${result}`);
